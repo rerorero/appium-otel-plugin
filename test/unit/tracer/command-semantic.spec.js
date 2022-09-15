77a5d5e0e796adff770e58ca5518955384004f55
rerorero <nato9598@hotmail.co.jp>
2022-09-16 00:29:33 +0900

    chore: add test for command-semantics

---
+++ test/unit/tracer/command-semantic.spec.js
@@ -0,0 +1,94 @@
+const { CommandTracerDelegator, } = require('../../../lib/trace/command-semantic');
+const {
+  InMemorySpanExporter,
+  SimpleSpanProcessor,
+  BasicTracerProvider,
+} = require('@opentelemetry/sdk-trace-base');
+const { SpanStatusCode } = require('@opentelemetry/api');
+import chai from 'chai';
+const should = chai.should();
+const { assert } = chai;
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+
+describe('CommandTracerDelegator', function() {
+  let memoryExporter;
+  let provider;
+  let tracer;
+  let sut;
+  const sessionId = 'a-123';
+  const caps = {
+    'deviceName': 'dev',
+    'app': 'foo.apk',
+    'platformName': 'android',
+  };
+
+  beforeEach(function() {
+    memoryExporter = new InMemorySpanExporter();
+    provider = new BasicTracerProvider();
+    provider.addSpanProcessor(new SimpleSpanProcessor(memoryExporter));
+    tracer = provider.getTracer('default');
+    sut = new CommandTracerDelegator(tracer, ['deviceName', 'app']);
+  });
+
+
+  it('should put spans when receiving command', async function() {
+    // create session
+    // eslint-disable-next-line require-await
+    let next = async function next() { return { value: [sessionId, caps] }; };
+    let res = await sut.delegateCreateSession(next, null, null, null, null);
+    assert.deepEqual(res, { value: [sessionId, caps] });
+
+    // command success
+    res = await sut.delegateHandle(
+      // eslint-disable-next-line require-await
+      async function next() { return 'success'; },
+      { sessionId }, 'cmd1', 'arg1', 'arg2');
+    assert.deepEqual(res, 'success');
+
+    // command failure
+    await expect(
+      sut.delegateHandle(
+        // eslint-disable-next-line require-await
+        async function next() { throw new Error('err'); },
+        { sessionId }, 'cmd2')
+    ).to.be.rejectedWith('err')
+
+    // non session command 
+    res = await sut.delegateHandle(
+      // eslint-disable-next-line require-await
+      async function next() { return 'success'; },
+      {}, 'cmd3', 'arg1');
+    assert.deepEqual(res, 'success');
+
+    // delete session
+    res = await sut.delegateDeleteSession(
+      // eslint-disable-next-line require-await
+      async function next() { return 'done'; },
+      null, null);
+    assert.deepEqual(res, 'done');
+
+    const spans = memoryExporter.getFinishedSpans();
+    should.equal(6, spans.length);
+    const expectedAttr = {
+      ['appium.caps.app']: 'foo.apk',
+      ['appium.caps.device_name']: 'dev',
+      ['appium.session.id']: sessionId,
+    };
+    const expected = [
+      { name: 'createSession', attr: expectedAttr, status: { code: SpanStatusCode.OK } },
+      { name: 'cmd1', attr: expectedAttr, status: { code: SpanStatusCode.OK } },
+      { name: 'cmd2', attr: expectedAttr, status: { code: SpanStatusCode.ERROR, message: 'Error: err' } },
+      { name: 'cmd3', attr: expectedAttr, status: { code: SpanStatusCode.OK } },
+      { name: 'deleteSession', attr: expectedAttr, status: { code: SpanStatusCode.OK } },
+      { name: 'session', attr: expectedAttr, status: { code: SpanStatusCode.UNSET } },
+    ];
+    expected.forEach((e, i) => {
+      assert.deepEqual(e.name, spans[i].name);
+      assert.deepEqual(e.attr, spans[i].attributes);
+      assert.deepEqual(e.status, spans[i].status);
+    });
+  });
+});
